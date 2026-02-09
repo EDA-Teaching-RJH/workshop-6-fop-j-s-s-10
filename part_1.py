40a4c847d62489987e76950c7e860ae26e38e77d
j-s-s-10 <jss10@kent.ac.uk>
2026-02-09 11:08:49 +0000

    Answered part 1

--- part_1.py
+++ part_1.py
@@ -1,1 +1,18 @@
+sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
+print(sample_bay[0])
+print(sample_bay[-1])
+print(len(sample_bay))
 
+for sample in sample_bay:
+    print(f"Transmitting data for: {sample}")
+
+new_findings = []
+i = 3
+while i:
+    i -= 1
+    new_findings.append(input("Enter rock type: "))
+print(new_findings)
+
+if "Dust" in sample_bay:
+    sample_bay.remove("Dust")
+print(sample_bay)
