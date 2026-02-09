78553a970f6593755cb4326467484b888525f432
j-s-s-10 <jss10@kent.ac.uk>
2026-02-09 11:30:09 +0000

    Answered part 3

--- part_3.py
+++ part_3.py
@@ -1,1 +1,20 @@
+try:
+    speed = int(input("Enter Motor Speed: "))
+    print("Speed set to", speed)
+except ValueError:
+    print("Error: Corrupted Signal. Maintaining current speed.")
 
+def get_coordinate():
+    while True:
+        try:
+            x = int(input("Enter x coordinate: "))
+            if abs(x) > 100:
+                print("Coordinate out of range")
+                raise Error
+        except:
+            print("Invalid coordinate")
+        else:
+            break
+    return x
+
+get_coordinate()
