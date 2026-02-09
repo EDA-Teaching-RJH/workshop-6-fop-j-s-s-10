2a82b6a18760e0f4baf57fdcd6460f846f8c3432
j-s-s-10 <jss10@kent.ac.uk>
2026-02-09 11:37:20 +0000

    Answered part 4

--- part_4.py
+++ part_4.py
@@ -1,1 +1,17 @@
+travel_log = []
 
+while True:
+    try:
+        slope_angle = int(input("Sensor Reading (Slope Angle): "))
+    except ValueError:
+        print("Sensor Glitch")
+    else:
+        if slope_angle > 45:
+            print("CRITICAL TILT! HALTING.")
+            break
+        travel_log.append(slope_angle)
+        print("Path Stable. Moving forward.")
+
+print("Mission Terminated.")
+print("Total steps taken:", len(travel_log))
+print(travel_log)
