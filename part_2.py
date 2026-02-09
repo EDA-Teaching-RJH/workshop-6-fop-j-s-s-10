32c514208d5fd7ac5764dd0690360e15e7e8d193
j-s-s-10 <jss10@kent.ac.uk>
2026-02-09 11:19:24 +0000

    Answered part 2

--- part_2.py
+++ part_2.py
@@ -1,1 +1,19 @@
+rover_status = {
+    "Battery": 100,
+    "Heater": "Off",
+    "Camera": "Standby"
+}
 
+print(rover_status["Battery"])
+
+rover_status["Battery"] = 85
+rover_status["Heater"] = "On"
+rover_status["Speed"] = 5
+print(rover_status)
+
+mission_log = [
+    {"Site": "Crater A", "Radiation": "Low", "Water": False},
+    {"Site": "Dune B", "Radiation": "High", "Water": True}
+]
+for i in mission_log:
+    print(f"Site {i["Site"]} has {i["Radiation"]} radiation")
